686660353085a0bfd5cd584c1bacbf8abbfc3ae2
Nandeesh Bhatrai <ninad.bhatrai1@gmail.com>
2023-09-23 22:15:17 +0530

    Added MyApp

---
+++ android/app/src/main/kotlin/com/example/six_pixel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.six_pixel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/kali/Desktop/Flutter/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/kali/Desktop/six_pixel" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.0.1+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/kali/Desktop/Flutter/flutter"
+  "PROJECT_DIR=/home/kali/Desktop/six_pixel"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC85MDY0NDU5YThiMGRjZDMyODc3MTA3ZjYwMDJjYzQyOWE3MTY1OWQxLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/kali/Desktop/six_pixel/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/kali/Desktop/six_pixel/lib/main.dart"
+)
